3646502959f641a586b1fb6d4e790e8ee0f36c1d
cloudthunder <kchenpc@gmail.com>
2017-10-28 13:30:29 -0700

    Initial commit

---
+++ swift practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift practice
+//
+//  Created by UCode on 10/28/17.
+//  Copyright © 2017 Kevin Chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
